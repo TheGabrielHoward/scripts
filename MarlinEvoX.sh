b0a167ac08359bc1d89bb55d9dacd1b19b4d8432
Gabriel Howard <j0s3ghoward@gmail.com>
2020-01-03 17:46:53 +0100

    Evolution X: Add Marlin & Salfish

---
+++ MarlinEvoX.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Run this on the main ROM folder
+
+# Repos
+git clone https://github.com/TheGabrielHoward/device_google_marlin -b ten device/google/marlin
+git clone https://github.com/shagbag913/lineage_kernel_google_marlin -b lineage-17.0 kernel/google/marlin
+git clone https://github.com/TheGabrielHoward/vendor_google -b ten vendor/google
+git clone https://github.com/DerpLab/platform_prebuilts_clang_host_linux-x86 -b ten clang/host/linux-x86
+
+# Starting Builds
+. b*/e*
+lunch aosp_marlin-userdebug
+mka bacon
